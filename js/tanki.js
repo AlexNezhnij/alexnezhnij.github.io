4b88368edc1a71cf0c20e17b369aed0ae7c7339b
AlexNezhnij <140158749+AlexNezhnij@users.noreply.github.com>
2023-08-01 15:00:32 +0400

    add tankiBodi and tankiMain, made function makes height of tankiMain 3/4 of width

--- js/tanki.js
+++ js/tanki.js
@@ -2,3 +2,16 @@
 
 console.log("fuuuuuuuuuuuuuuuuu")
 applyBodyClass(localStorage.getItem('bodyClass'));
+
+
+function setDivHeightToThreeFourth() {
+    const tankiMain = document.getElementById('tankiMain')
+    const width = tankiMain.clientWidth;
+    const height = (3 / 4) * width; // Высота равна 3/4 от ширины
+  
+    tankiMain.style.height = height + 'px';
+    console.log('fooooo');
+    
+  }
+  setDivHeightToThreeFourth()
+  window.addEventListener("resize", setDivHeightToThreeFourth);
