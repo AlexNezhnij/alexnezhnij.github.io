3a9b3773b5a2101ef6e1d9d7f729a6e46e621cd9
AlexNezhnij <140158749+AlexNezhnij@users.noreply.github.com>
2023-08-03 17:23:24 +0400

    add movement of pixel1 in tankiMain in tanki.js

--- js/tanki.js
+++ js/tanki.js
@@ -15,3 +15,36 @@ function setDivHeightToThreeFourth() {
   }
   setDivHeightToThreeFourth()
   window.addEventListener("resize", setDivHeightToThreeFourth);
+
+  const pixel1 = document.getElementById("pixel1");
+
+  var cordinatX = 0;
+  var cordinatY = 0;
+
+  document.addEventListener("keydown", function(event) {
+
+    if (event.key === "ArrowUp") {
+      cordinatY -= 2;
+      pixel1.style.transform = `translate(${cordinatX}px, ${cordinatY}px)`;
+     
+    } else if (event.key === "ArrowDown") {
+      cordinatY += 2;
+      pixel1.style.transform = `translate(${cordinatX}px, ${cordinatY}px)`;
+      
+    } else if (event.key === "ArrowLeft") {
+      cordinatX -= 2;
+      pixel1.style.transform = `translate(${cordinatX}px, ${cordinatY}px)`;
+     
+    } else if (event.key === "ArrowRight") {
+      cordinatX += 2;
+      pixel1.style.transform = `translate(${cordinatX}px, ${cordinatY}px)`;
+      
+    }
+  });
+  
+  
+  
+  
+  
+  
+  
